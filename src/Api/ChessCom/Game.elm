be965c3d7467a0656b4b3fb9d7138eb014accf9d
Shea Newton <shnewto@gmail.com>
2020-08-30 17:01:32 -0600

    save state, crawling through migrating old logic to the new elm-spa setup

---
+++ src/Api/ChessCom/Game.elm
@@ -0,0 +1,156 @@
+module Api.ChessCom.Game exposing (..)
+
+import Api.ChessCom.Player exposing (Player, playerDecoder)
+import Api.Data as Api
+import Http
+import Json.Decode as Decode exposing (Decoder, int, list, nullable, string)
+import Json.Decode.Pipeline as Pipeline exposing (optional, required, requiredAt, resolve)
+
+
+{-| Data Format, each Game:
+
+{
+"white": { // details of the white-piece player:
+"username": "string", // the username
+"rating": 1492, // the player's rating at the start of the game
+"result": "string", // see "Game results codes" section
+"@id": "string", // URL of this player's profile
+},
+"black": { // details of the black-piece player:
+"username": "string", // the username
+"rating": 1942, // the player's rating at the start of the game
+"result": "string", // see "Game results codes" section
+"@id": "string", // URL of this player's profile
+},
+"url": "string", // URL of this game
+"fen": "string", // final FEN
+"pgn": "string", // final PGN
+"start\_time": 1254438881, // timestamp of the game start (Daily Chess only)
+"end\_time": 1254670734, // timestamp of the game end
+"time\_control": "string", // PGN-compliant time control
+"rules": "string", // game variant information (e.g., "chess960")
+"eco": "string", //URL pointing to ECO opening (if available),
+"tournament": "string", //URL pointing to tournament (if available),
+"match": "string", //URL pointing to team match (if available)
+}
+
+-}
+type alias Game =
+    { white : Player
+    , black : Player
+    , url : String
+    , fen : String
+    , pgn : String
+    , startTime : Maybe Int
+    , endTime : Maybe Int
+    , timeControl : Maybe String
+    , rules : String
+    , eco : Maybe String
+    , tournament : Maybe String
+    , match : Maybe String
+    }
+
+
+type alias GameMonth =
+    { month : Maybe String
+    , year : Maybe String
+    , games : List Game
+    , visible : Bool
+    , url : String
+    , username : String
+    }
+
+
+gameURL : String -> String -> String
+gameURL username date =
+    "https://api.chess.com/pub/player/" ++ username ++ "/games/" ++ date
+
+
+gamesURL : String -> String
+gamesURL username =
+    "https://api.chess.com/pub/player/" ++ username ++ "/games/archives"
+
+
+getGamesURLs : String -> { onResponse : Api.Data (List String) -> msg } -> Cmd msg
+getGamesURLs username options =
+    Http.get
+        { url = gamesURL username
+        , expect =
+            Api.expectJson options.onResponse gamesURLsDecoder
+        }
+
+
+getGamesInMonth : String -> String -> { onResponse : Api.Data (List Game) -> msg } -> Cmd msg
+getGamesInMonth username url options =
+    Http.get
+        { url = url
+        , expect = Api.expectJson options.onResponse gamesDecoder
+        }
+
+
+getGameMonths : String -> List String -> { onResponse : Api.Data (List Game) -> msg } -> Cmd msg
+getGameMonths username urls options =
+    let
+        allGamesInMonths =
+            List.map (\url -> getGamesInMonth url username options) urls
+    in
+    Cmd.batch allGamesInMonths
+
+
+{-| Data Format:
+
+{
+"archives": [
+/* array of URLs for monthly archives in ascending chronological order */
+]
+}
+
+URL pattern: <https://api.chess.com/pub/player/{username}/games/{YYYY}/{MM}>
+
+"YYYY" is the four digit year of the game-end
+"MM" is the two-digit month
+
+-}
+gamesURLsDecoder : Decode.Decoder (List String)
+gamesURLsDecoder =
+    let
+        toDecoder : List String -> Decode.Decoder (List String)
+        toDecoder games =
+            Decode.succeed games
+    in
+    Decode.succeed toDecoder
+        |> Pipeline.required "archives" (Decode.list Decode.string)
+        |> Pipeline.resolve
+
+
+{-| URL pattern: <https://api.chess.com/pub/player/{username}/games/{YYYY}/{MM}>
+-}
+gameDecoder : Decoder Game
+gameDecoder =
+    Decode.succeed Game
+        |> required "white" playerDecoder
+        |> required "black" playerDecoder
+        |> required "url" string
+        |> required "fen" string
+        |> required "pgn" string
+        |> optional "startTime" (nullable int) Nothing
+        |> optional "endTime" (nullable int) Nothing
+        |> optional "timeControl" (nullable string) Nothing
+        |> required "rules" string
+        |> optional "eco" (nullable string) Nothing
+        |> optional "tournament" (nullable string) Nothing
+        |> optional "match" (nullable string) Nothing
+
+
+{-| "games": [/* array of game objects */]
+-}
+gamesDecoder : Decoder (List Game)
+gamesDecoder =
+    let
+        toDecoder : List Game -> Decoder (List Game)
+        toDecoder games =
+            Decode.succeed games
+    in
+    Decode.succeed toDecoder
+        |> requiredAt [ "games" ] (list gameDecoder)
+        |> resolve
--- src/Pages/Top.elm
+++ src/Pages/Top.elm
@@ -1,12 +1,43 @@
 module Pages.Top exposing (Model, Msg, Params, page)
 
+import Api.ChessCom.Game exposing (Game, GameMonth)
+import Api.Data exposing (Data(..))
+import Dict exposing (Dict)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Misc exposing (onKeyUp)
+import Http
+import Misc exposing (gamePathFromUrl, monthNumberToMonthName, onKeyUp, resultCodeDescription)
+import Pages.NotFound exposing (Params)
+import Shared
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url exposing (Url)
+
+
+type Msg
+    = KeyUp Int
+    | UsernameEntered String
+    | UsernameLoaded
+    | ToggleSectionVisible String
+    | GameURLsLoaded (Data (List String))
+    | GameMonthsLoaded (Data (List Game))
+
+
+page : Page Params Model Msg
+page =
+    Page.application
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        , save = save
+        , load = load
+        }
+
+
+
+-- INIT
 
 
 type alias Params =
@@ -14,39 +45,166 @@ type alias Params =
 
 
 type alias Model =
-    Url Params
+    { url : Url Params
+    , username : Maybe String
+    , gameUrls : Data (List String)
+    , errors : List String
+    , gameMonths : Data (Dict String GameMonth)
+    }
 
 
-type Msg
-    = Never
-    | KeyUp Int
-    | UsernameEntered String
-    | UsernameLoaded
+init : Shared.Model -> Url Params -> ( Model, Cmd Msg )
+init shared url =
+    let
+        username =
+            Dict.get "username" url.query
+
+        dataState =
+            if username == Nothing then
+                NotAsked
+
+            else
+                Loading
+
+        fetchGamesCmd =
+            case username of
+                Just name ->
+                    fetchGameUrls name
+
+                Nothing ->
+                    Cmd.none
+
+        model : Model
+        model =
+            { url = url
+            , username = username
+            , gameUrls = dataState
+            , gameMonths = dataState
+            , errors = []
+            }
+    in
+    ( model
+    , Cmd.batch
+        [ fetchGamesCmd
+        ]
+    )
 
 
-page : Page Params Model Msg
-page =
-    Page.static
-        { view = view
+fetchGameUrls : String -> Cmd Msg
+fetchGameUrls username =
+    Api.ChessCom.Game.getGamesURLs username
+        { onResponse = GameURLsLoaded
         }
+
+
+fetchGameMonths : Model -> List String -> Cmd Msg
+fetchGameMonths model =
+    case ( model.username, model.gameUrls ) of
+        ( Just username, Success urls ) ->
+            Api.ChessCom.Game.getGameMonths username urls { onResponse = GameMonthsLoaded }
+
+        _ ->
+            Cmd.none
+
+
+
+-- Update
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        usernameLoaded keycode =
+            let
+                gotUserName username =
+                    ( { model | username = Just username }
+                    , Cmd.batch
+                        [ fetchGameUrls username
+                        ]
+                    )
+            in
+            case ( model.username, keycode ) of
+                ( Just username, Nothing ) ->
+                    gotUserName username
+
+                ( Just username, Just code ) ->
+                    if code == 13 then
+                        gotUserName username
+
+                    else
+                        ( model, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+    in
+    case msg of
+        KeyUp keycode ->
+            usernameLoaded <| Just keycode
+
+        UsernameEntered username ->
+            ( { model
+                | username = Just username
+              }
+            , Cmd.none
+            )
+
+        UsernameLoaded ->
+            usernameLoaded Nothing
+
+        ToggleSectionVisible s ->
+            ( model, Cmd.none )
+
+        GameURLsLoaded gameUrls ->
+            ( { model | gameUrls = gameUrls }, fetchGameMonths { model | gameUrls = gameUrls } )
+
+        GameMonthsLoaded gameMonths ->
+            { model | gameMonths = gameMonths }
+
+
+save : Model -> Shared.Model -> Shared.Model
+save _ shared =
+    shared
+
+
+load : Shared.Model -> Model -> ( Model, Cmd Msg )
+load _ model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 
 
 -- VIEW
 
 
-view : Url Params -> Document Msg
-view { params } =
-    { title = "Homepage"
-    , body = [ inputView Nothing ]
+view : Model -> Document Msg
+view model =
+    { title = "Search"
+    , body = [ inputView model.username ]
     }
 
 
-searchView : Maybe String -> Html Msg
-searchView username =
-    div [ class "archives-body" ]
-        [ inputView username
-        , p [] [{- archives -}]
+searchView : Model -> Html Msg
+searchView model =
+    let
+        gamesElement =
+            case model.gameMonths of
+                Success allGames ->
+                    div [] <|
+                        List.map
+                            (\( _, gameMonth ) -> gameMonthElement gameMonth)
+                        <|
+                            Dict.toList allGames
+
+                _ ->
+                    div [] []
+    in
+    div [ class "games-body" ]
+        [ inputView model.username
+        , p [] [ gamesElement ]
         ]
 
 
@@ -71,3 +229,77 @@ inputView placeholderText =
             ]
             []
         ]
+
+
+gameMonthElement : GameMonth -> Html Msg
+gameMonthElement m =
+    let
+        monthElement =
+            let
+                ( icon, active ) =
+                    if m.visible then
+                        ( String.fromChar (Char.fromCode 0x2182), " active" )
+
+                    else
+                        ( String.fromChar (Char.fromCode 0x2180), "" )
+            in
+            case ( m.month, m.year ) of
+                ( Just month, Just year ) ->
+                    button
+                        [ class <| "collapsible" ++ active
+                        , onClick <| ToggleSectionVisible m.url
+                        ]
+                        [ b [ class "collapsible-icon" ] [ text icon ]
+                        , mark [ class "month" ]
+                            [ text <|
+                                monthNumberToMonthName month
+                                    ++ " "
+                                    ++ year
+                            ]
+                        ]
+
+                _ ->
+                    div [] []
+
+        gamesElement =
+            List.map
+                (\game ->
+                    let
+                        ( userPlayer, opponent ) =
+                            if game.white.username == m.username then
+                                ( game.white, game.black )
+
+                            else
+                                ( game.black, game.white )
+                    in
+                    div [ class "harpoon" ]
+                        [ a [ class "game-url", href <| gamePathFromUrl userPlayer.username game ]
+                            [ text <|
+                                userPlayer.username
+                                    ++ " ("
+                                    ++ String.fromInt userPlayer.rating
+                                    ++ ")"
+                                    ++ " vs. "
+                                    ++ opponent.username
+                                    ++ " ("
+                                    ++ String.fromInt opponent.rating
+                                    ++ ") : \""
+                                    ++ resultCodeDescription userPlayer.result
+                                    ++ "\""
+                            ]
+                        ]
+                )
+                m.games
+
+        contentElement =
+            let
+                active =
+                    if m.visible then
+                        "-active"
+
+                    else
+                        "-hidden"
+            in
+            div [ class <| "content" ++ active ] [ div [ class "games" ] gamesElement ]
+    in
+    div [ class "" ] [ monthElement, contentElement ]
